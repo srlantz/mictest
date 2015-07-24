a22de48ee0a7fcfc210808f96a03112326e7fee2
cerati <giuseppe.cerati@cern.ch>
2015-07-24 00:59:01 -0700

    propagating fixes to this branch

--- mkFit/HitStructures.h
+++ mkFit/HitStructures.h
@@ -118,6 +118,22 @@ public:
       last_bin = bin;
       ++idx;
     }
+
+    //now fix empty bins
+    int nextHitToFind = 0;
+    for (int b = 0; b < Config::nPhiPart; ++b)
+    {
+      if (m_phi_bin_infos[b].first == 0 && m_phi_bin_infos[b].second == 0) 
+      {
+	m_phi_bin_infos[b].first = nextHitToFind;
+      } 
+      else 
+      {
+	nextHitToFind = m_phi_bin_infos[b].first + m_phi_bin_infos[b].second;
+      }
+      //std::cout << "bin=" << b << " set to " << m_phi_bin_infos[b].first << "," << m_phi_bin_infos[b].second << std::endl;
+    }
+
   }
 };
 
