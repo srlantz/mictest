5175b5ff0bf0872db5b99d384e188eb3d2901485
Giuseppe Cerati <giuseppe.cerati@cern.ch>
2015-09-21 22:46:41 +0200

    new files

---
+++ mkFit/testPropagation.cc
@@ -0,0 +1,28 @@
+#include "testPropagation.h"
+#include <iostream>
+
+#include "PropagationMPlex.h"
+#include "MkFitter.h"
+
+#include <omp.h>
+
+void testPropagation(Event& ev) {
+
+  std::cout << "CIAO" << std::endl;
+
+  std::vector<Track>& simtracks = ev.simTracks_;
+  
+  MkFitter *mkfp = new (_mm_malloc(sizeof(MkFitter), 64)) MkFitter(10);
+  
+  for (int itrack = 0; itrack < simtracks.size(); itrack += NN)
+    {
+      int end = std::min(itrack + NN, int(simtracks.size()));
+      
+      mkfp->InputTracksAndHits(simtracks, ev.layerHits_, itrack, end);
+
+      mkfp->TestPropagation();      
+    }
+  
+  return;
+  
+}
---
+++ mkFit/testPropagation.h
@@ -0,0 +1,8 @@
+#ifndef _testPropagation_h_
+#define _testPropagation_h_
+
+#include "Event.h"
+
+void testPropagation(Event& ev);
+
+#endif
